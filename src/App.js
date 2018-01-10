69960d13c2fbe91b156ebebe02d6510225609edd
nurkan <nice@nurkans-MacBook-Air.local>
2018-01-10 18:53:38 +0600

    add props

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 const data = require('./data.json');
 
+
+const Movies = props => (
+    <div className="cont">
+        <div className="header">
+            <h1>{props.show.name}</h1>
+        </div>
+        <img src={`/public/img/posters/${props.show.img}`} alt={props.show.name} height="40" width="70" />
+        <p>Year: {props.show.year}</p>
+    </div>
+);
+
+
 class App extends Component {
   render() {
     return (
         data.movies.map(
             show => (
-                <div className="cont">
-                    <div className="header">
-                        <h1>{show.name}</h1>
-                    </div>
-                    <img src={`/public/img/posters/${show.img}`} alt={show.name} height="40" width="70" />
-                    <p>Year: {show.year}</p>
-                </div>
+                <Movies show={show}/>
             )
         )
     )
